825570e43a724290dcf58fc90594c9f60753cea3
Amruta Joagudri <amrutajodagudri@gmail.com>
2024-12-20 10:39:27 +0530

    Help Support page done

--- src/components/HelpSupportPage.jsx
+++ src/components/HelpSupportPage.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import "./HelpSupportPage.css";
 import { GrLocation } from "react-icons/gr";
-import {
-  BsChatDotsFill,
-  BsChatLeftText,
-  BsChatRight,
-  BsChatRightText,
-} from "react-icons/bs";
-import { IoChatbox, IoShareSocialOutline } from "react-icons/io5";
-import { TiSocialAtCircular } from "react-icons/ti";
+import { BsChatRightText } from "react-icons/bs";
 import { MdOutlineSocialDistance } from "react-icons/md";
 import { LuMailCheck } from "react-icons/lu";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { FaArrowRightToBracket } from "react-icons/fa6";
 
 const HelpAndSupportPage = () => {
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [isIconsVisible, setIconsVisible] = useState(false);
+  const [isIconVisible, setIconVisible] = useState(false);
+
+  const toggleIconsVisibility = () => {
+    setIconsVisible(!isIconsVisible);
+  };
+
+  const toggleIconVisibility = () => {
+    setIconVisible(!isIconVisible);
+  };
+
+  const toggleFormVisibility = () => {
+    setFormVisible(!isFormVisible);
+  };
+
   return (
     <div className="help-support-container">
       <Navbar />
@@ -24,17 +35,19 @@ const HelpAndSupportPage = () => {
         <section className="help-options">
           <div className="option-card">
             <div className="icon1">❓</div>
-            <div className="text-content">
+            <div className="title-text">
               <h2>FAQ</h2>
               <p>Lorem ipsum dolor sit amet</p>
             </div>
+            <FaArrowRightToBracket className="small-screen-icon" />
           </div>
           <div className="option-card">
             <GrLocation className="profile-icon-large" />
-            <div className="text-content">
+            <div className="title-text">
               <h2>Travel Guide</h2>
               <p>Lorem ipsum dolor sit amet</p>
             </div>
+              <FaArrowRightToBracket className="small-screen-icon" />
           </div>
         </section>
 
@@ -45,21 +58,26 @@ const HelpAndSupportPage = () => {
               <h2>Chat</h2>
               <p>Lorem ipsum dolor sit amet</p>
             </div>
+            <button className="toggle-button" onClick={toggleIconsVisibility}>
+              {isIconsVisible ? <FiChevronUp /> : <FiChevronDown />}
+            </button>
           </div>
-          <div className="social-icons">
-            <a href="#" className="chat-icon telegram">
-              <img src="/telegram-icon.svg" alt="Telegram" />
-              <p>Telegram</p>
-            </a>
-            <a href="#" className="chat-icon whatsapp">
-              <img src="/whatsapp-icon.svg" alt="WhatsApp" />
-              <p>WhatsApp</p>
-            </a>
-            <a href="#" className="chat-icon livechat">
-              <img src="/live-chat-icon.svg" alt="Live Chat" />
-              <p>Live Chat</p>
-            </a>
-          </div>
+          {isIconsVisible && (
+            <div className="social-icons">
+              <a href="#" className="chat-icon telegram">
+                <img src="/Social-media-icons/telegram.png" alt="Telegram" />
+                <p>Telegram</p>
+              </a>
+              <a href="#" className="chat-icon whatsapp">
+                <img src="/Social-media-icons/whatsapp.png" alt="WhatsApp" />
+                <p>WhatsApp</p>
+              </a>
+              <a href="#" className="chat-icon chat">
+                <img src="/Social-media-icons/chatlogo.webp" alt="Live Chat" />
+                <p>Live Chat</p>
+              </a>
+            </div>
+          )}
         </section>
 
         <section className="expandable-section">
@@ -69,21 +87,26 @@ const HelpAndSupportPage = () => {
               <h2>Social</h2>
               <p>Lorem ipsum dolor sit amet</p>
             </div>
+            <button className="toggle-button" onClick={toggleIconVisibility}>
+              {isIconVisible ? <FiChevronUp /> : <FiChevronDown />}
+            </button>
           </div>
-          <div className="social-icons">
-            <a href="#" className="chat-icon telegram">
-              <img src="/facebook-icon.svg" alt="facebook" />
-            </a>
-            <a href="#" className="chat-icon whatsapp">
-              <img src="/instagram-icon.svg" alt="instagram" />
-            </a>
-            <a href="#" className="chat-icon livechat">
-              <img src="/twitter-icon.svg" alt="twitter" />
-            </a>
-            <a href="#" className="chat-icon livechat">
-              <img src="/linkedin-icon.svg" alt="linkedin" />
-            </a>
-          </div>
+          {isIconVisible && (
+            <div className="social-icons">
+              <a href="#" className="chat-icon telegram">
+                <img src="/Social-media-icons/facebook.png" alt="facebook" />
+              </a>
+              <a href="#" className="chat-icon whatsapp">
+                <img src="/Social-media-icons/instagram.png" alt="instagram" />
+              </a>
+              <a href="#" className="chat-icon twitter">
+                <img src="/Social-media-icons/telegram.png" alt="twitter" />
+              </a>
+              <a href="#" className="chat-icon linkedin">
+                <img src="/Social-media-icons/linkedin.png" alt="linkedin" />
+              </a>
+            </div>
+          )}
         </section>
 
         <section className="expandable-section">
@@ -93,28 +116,41 @@ const HelpAndSupportPage = () => {
               <h2>Send Query</h2>
               <p>Lorem ipsum dolor sit amet</p>
             </div>
-          </div>
-          <form className="query-form">
-            <div className="form-row">
-              <input type="text" placeholder="Name" className="form-input" />
-              <input type="email" placeholder="Email" className="form-input" />
-            </div>
-            <div className="form-row">
-              <input type="text" placeholder="Subject" className="form-input" />
-              <input
-                type="text"
-                placeholder="Order No."
-                className="form-input"
-              />
-            </div>
-            <textarea
-              placeholder="Your Query"
-              className="form-textarea"
-            ></textarea>
-            <button type="submit" className="submit-button">
-              Submit
+            <button className="toggle-button" onClick={toggleFormVisibility}>
+              {isFormVisible ? <FiChevronUp /> : <FiChevronDown />}
             </button>
-          </form>
+          </div>
+          {isFormVisible && (
+            <form className="query-form">
+              <div className="form-row">
+                <input type="text" placeholder="Name" className="form-input" />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="form-input"
+                />
+              </div>
+              <div className="form-row">
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="form-input"
+                />
+                <input
+                  type="text"
+                  placeholder="Order No."
+                  className="form-input"
+                />
+              </div>
+              <textarea
+                placeholder="Your Query"
+                className="form-textarea"
+              ></textarea>
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </form>
+          )}
         </section>
       </main>
     </div>
